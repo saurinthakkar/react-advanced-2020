6d11837a8e5c852de1ff7f36d6c7f7e60f0c2d8b
Saurin Thakkar <saurin.thakkar13@gmail.com>
2022-04-02 15:28:24 +0530

    Completed till short circuit logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-import Setup from "./tutorial/1-useState/setup/5-useState-counter";
+import Setup from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
 //import Final from "./tutorial/1-useState/final/1-error-example";
 
 function App() {
@@ -8,15 +7,6 @@ function App() {
     <React.Fragment>
       <Setup />
     </React.Fragment>
-=======
-import Setup from "./tutorial/1-useState/setup/1-error-example";
-
-function App() {
-  return (
-    <div className="container">
-      <Setup />
-    </div>
->>>>>>> 124ddb70b11b9a6f883e4d4a90b0270cdd58250f
   );
 }
 
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("Petr");
 
-  return <h2>short circuit</h2>;
+  //const firstValue = text || "hello world";
+  const secondValue = text && "hello second value";
+  console.log(secondValue);
+
+  return (
+    <>
+      <h1>{text || "john doe"}</h1>
+      {!text && <h1>2nd argument in and operator</h1>}
+    </>
+  );
 };
 
 export default ShortCircuit;
