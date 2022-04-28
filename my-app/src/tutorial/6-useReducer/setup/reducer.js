e71e3a889d40b0c3c9a7f04f05036357079f1d97
Saurin Thakkar <saurin.thakkar13@gmail.com>
2022-04-28 23:15:45 +0530

    completed until useReducer component

--- my-app/src/tutorial/6-useReducer/setup/reducer.js
+++ my-app/src/tutorial/6-useReducer/setup/reducer.js
@@ -1,4 +1,6 @@
 export const reducer = (state, action) => {
+  //console.log(state);
+  // console.log(state, action);
   if (action.type === "ADD_ITEM") {
     const newPeople = [...state.people, action.payload];
     return {
@@ -7,9 +9,15 @@ export const reducer = (state, action) => {
       isModalOpen: true,
       modalContent: "item added",
     };
+    //console.log(state);
   }
+
   if (action.type === "NO_VALUE") {
-    return { ...state, isModalOpen: true, modalContent: "please enter value" };
+    return {
+      ...state,
+      isModalOpen: true,
+      modalContent: "please Enter a value",
+    };
   }
   if (action.type === "CLOSE_MODAL") {
     return { ...state, isModalOpen: false };
@@ -19,7 +27,10 @@ export const reducer = (state, action) => {
     const newPeople = state.people.filter(
       (person) => person.id !== action.payload
     );
+    // console.log(newPeople);
     return { ...state, people: newPeople };
   }
-  throw new Error("no matching action type");
+  throw new Error("no matching found");
 };
+
+export default reducer;
